835eaa27582aa7aa7161e92a5c4f8d77ebc27b1c
Aurelius84 <liujiezhangbupt@gmail.com>
2020-04-16 09:17:33 +0800

    modify file type

--- cpp/cJSON/cJSON.c
+++ cpp/cJSON/cJSON.c
@@ -2,12 +2,13 @@
 // Created by zhangliujie on 2020/4/15.
 //
 
-#include <string>
-#include <cstdio>
-#include <cmath>
-#include <cfloat>
-#include <cctype>
-#include <climits>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <float.h>
+#include <ctype.h>
+#include <limits.h>
 #include "cJSON.h"
 
 /*
@@ -21,6 +22,12 @@ static const char *ep;
 
 /* Parse the input text into an unescaped cstring, and populate item. */
 static const unsigned char firstByteMask[7] = {0x00, 0x00, 0xC0, 0xE0, 0xF0, 0xF8, 0xFC};
+
+static const char *parse_value(cJSON *item, const char *value);
+
+static const char *parse_array(cJSON *item, const char *value);
+
+static const char *parse_object(cJSON *item, const char *value);
 
 /* malloc和new的区别:
  * 1. new可以自动计算申请内存的大小, malloc需指定size; 如 int *p = new int[10]
@@ -62,6 +69,33 @@ static const char *skip(const char *in){
     return in;
 }
 
+static unsigned parse_hex4(const char *str) {
+    unsigned h = 0;
+    if (*str >= '0' && *str <= '9') h += (*str) - '0';
+    else if (*str >= 'A' && *str <= 'F')
+        h += 10 + (*str) - 'A';
+    else if (*str >= 'a' && *str <= 'f') h += 10 + (*str) - 'a'; else return 0;
+    h = h << 4;
+    str++;
+    if (*str >= '0' && *str <= '9') h += (*str) - '0';
+    else if (*str >= 'A' && *str <= 'F')
+        h += 10 + (*str) - 'A';
+    else if (*str >= 'a' && *str <= 'f') h += 10 + (*str) - 'a'; else return 0;
+    h = h << 4;
+    str++;
+    if (*str >= '0' && *str <= '9') h += (*str) - '0';
+    else if (*str >= 'A' && *str <= 'F')
+        h += 10 + (*str) - 'A';
+    else if (*str >= 'a' && *str <= 'f') h += 10 + (*str) - 'a'; else return 0;
+    h = h << 4;
+    str++;
+    if (*str >= '0' && *str <= '9') h += (*str) - '0';
+    else if (*str >= 'A' && *str <= 'F')
+        h += 10 + (*str) - 'A';
+    else if (*str >= 'a' && *str <= 'f') h += 10 + (*str) - 'a'; else return 0;
+    return h;
+}
+
 static const char* parse_string(cJSON *item, const char *str){
     // 忽略前面的"
     const char *ptr = str + 1;
@@ -72,14 +106,14 @@ static const char* parse_string(cJSON *item, const char *str){
     unsigned uc, uc2;
     if(*str != '\"'){
         ep = str;
-        return nullptr; // not a string
+        return 0; // not a string
     }
     while(*ptr != '\"' && *ptr && ++len){
         if(*ptr++ == '\\') ptr ++; // skip escaped quota
     }
 
     out = (char *) cJSON_malloc(len+1);
-    if(!out) return nullptr;
+    if(!out) return 0;
 
     ptr = str + 1;
     ptr2 = out;
@@ -130,12 +164,12 @@ static const char* parse_string(cJSON *item, const char *str){
                             uc >>=6;
                         case 3:
                             *--ptr2 = ((uc | 0x80) & 0xBF);
-                            uc >> 6;
+                            uc >>= 6;
                         case 2:
                             *--ptr2 = ((uc | 0x80) & 0xBF);
-                            uc >> 6;
+                            uc >>= 6;
                         case 1:
-                            *--ptr2 = (uc | fisrtByteMark[len]);
+                            *--ptr2 = (uc | firstByteMask[len]);
                     }
                     ptr2 += len;
                     break;
@@ -153,8 +187,56 @@ static const char* parse_string(cJSON *item, const char *str){
     return ptr;
 }
 
+const char *parse_number(cJSON *item, const char*num){
+    double n =0, sign = 1, scale = 0;
+    int subscale = 0, signsuscale = 1;
+
+    if(*num == '-'){
+        sign = -1;
+        num++;
+    }
+    if(*num == '0') num++;
+    if(*num >= '1' & *num <= '9') {
+        do n = (n * 10) + (*num++ -'0');
+        while (*num >='0' & *num <= '9');
+    }
+    if(*num == '.' && num[1] >='0' && num[1] <= '9'){
+        num++;
+        do n = (n * 10.0) + (*num++ - '0'), scale--;
+        while (*num >= '0' && *num <= '9');
+    }
+    if(*num == 'e' || *num =='E'){
+        num++;
+        if(*num =='+') num++;
+        else if(*num == '-'){
+            signsuscale = -1;
+            num++;
+        }
+        while(*num >='0' && *num <= '9')
+            subscale = (subscale*10) + (*num++ - '0');
+    }
+
+    n = sign * n * pow(10.0, (scale + subscale*signsuscale));
+
+    item->valuedouble = n;
+    item->valueint = (int) n;
+    item->type = cJSON_Number;
+    return num;
+}
+
+/*
+ * 在C语言中，由于没有静态成员一说，故static的作用主要有两个：
+ *   参考：https://blog.csdn.net/FreeApe/article/details/50979425
+ *   1. 隐藏功能，对于static修饰的函数和全局变量而言（避免了调用函数时的压栈和出栈）
+ *   2. 保持持久性功能
+ *
+ * 对于类静态成员函数：（参考：https://blog.csdn.net/danforn/article/details/2312766）
+ *   1. 没有this隐含指针
+ *   2. 不能在静态成员函数里访问类的非静态成员，因为实例化的对象存放在局部变量区，静态成员存在在全局变量区
+ *   3. 不能将静态成员函数定义为虚函数
+ */
 static const char *parse_value(cJSON *item, const char *value){
-    if(!value)  return nullptr;
+    if(!value)  return 0;
     /* 若前n个字符相同，则返回0
      * C++中 单引号是字符型(实际代表一个整数)，双引号是字符串型(指向吴明数组起始字符的指针)
      * 'a' 表示一个字符，"a"相当于'a' + '\0'
@@ -171,7 +253,7 @@ static const char *parse_value(cJSON *item, const char *value){
 
     if(!strncmp(value, "true", 4)){
         item->type = cJSON_True;
-        item->vauleint = 1;
+        item->valueint = 1;
         return value + 4;
     }
     // TODO: add parse function
@@ -181,38 +263,47 @@ static const char *parse_value(cJSON *item, const char *value){
     if(*value == '{') {return parse_object(item, value);}
 
     ep = value;
-    return nullptr; //fail
+    return 0; //fail
+}
+
+// TODO: add definations
+static const char *parse_array(cJSON *item, const char *value){
+    return 0;
+}
+
+// TODO: add definations
+static const char *parse_object(cJSON *item, const char *value){
+    return 0;
 }
 
 cJSON *cJSON_Parse(const char *value){
-    return cJSON_ParseWithOpts(value, nullptr, 0);
+    return cJSON_ParseWithOpts(value, 0, 0);
 }
 
 void cJSON_Delete(cJSON *c){
     cJSON *next;
     while(c){
         next = c->next;
-        if(!(c->type && cJSON_IsReference) && c->child) cJSON_Delete(c->child);
-        if(!(c->type && cJSON_IsReference) && c->valuestring) cJSON_free(c->valuestring);
-        if(!(c->type && cJSON_StringIsConst) && c->string) cJSON_free(c->string);
+        if(!(c->type & cJSON_IsReference) && c->child) cJSON_Delete(c->child);
+        if(!(c->type & cJSON_IsReference) && c->valuestring) cJSON_free(c->valuestring);
+        if(!(c->type & cJSON_StringIsConst) && c->string) cJSON_free(c->string);
         cJSON_free(c);
         c = next;
     }
 }
 
 cJSON *cJSON_ParseWithOpts(const char *value, const char **return_parse_end, int require_null_terminated) {
-    const char *end = nullptr;
+    const char *end = 0;
     cJSON *c = cJSON_New_Item();
-    ep = nullptr;
-    if(!c) return nullptr;  // memory fail
+    ep = 0;
+    if(!c) return 0;  // memory fail
 
     // return pointer pointing the end of char* value
     end = parse_value(c, skip(value));
 
     if(!end){
-        // TODO: add defination
         cJSON_Delete(c);
-        return nullptr; // parse failure, ep is set
+        return 0; // parse failure, ep is set
     }
 
     if(require_null_terminated){
@@ -220,7 +311,7 @@ cJSON *cJSON_ParseWithOpts(const char *value, const char **return_parse_end, int
         if(*end){
             cJSON_Delete(c);
             ep = end;
-            return nullptr;
+            return 0;
         }
     }
     if(return_parse_end) *return_parse_end = end;
--- cpp/cJSON/cJSON.h
+++ cpp/cJSON/cJSON.h
@@ -24,12 +24,21 @@ typedef struct cJSON {
     int type;
 
     char *valuestring;
-    int vauleint;
+    int valueint;
     double valuedouble;
 
     char *string;
 } cJSON;
 
+/*
+ * C语言中，利用extern，
+ *    1. 可以在一个文件中引用另一个文件定义的变量和函数
+ *    2. 该行只是声明，并没有分配内存
+ *    3. 场景：一个c文件需要调用另一个c文件中的变量和函数
+ * 参考：
+ *   1. https://blog.csdn.net/xingjiarong/article/details/47656339
+ *   2. http://www.uml.org.cn/c%2B%2B/201908011.asp
+ */
 extern cJSON *cJSON_Parse(const char *value);
 
 extern cJSON *cJSON_ParseWithOpts(const char *value, const char **return_parse_end, int require_null_terminated);
